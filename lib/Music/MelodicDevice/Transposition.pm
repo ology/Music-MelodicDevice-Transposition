2f1ae9f25271d2cfceb5e3e0853946236181c8f2
Gene Boggs <gene@ology.net>
2022-06-16 21:12:24 -0700

    Bump version

--- lib/Music/MelodicDevice/Transposition.pm
+++ lib/Music/MelodicDevice/Transposition.pm
@@ -2,7 +2,7 @@ package Music::MelodicDevice::Transposition;
 
 # ABSTRACT: Apply chromatic and diatonic transposition to notes
 
-our $VERSION = '0.0501';
+our $VERSION = '0.0502';
 
 use Data::Dumper::Compact qw(ddc);
 use List::SomeUtils qw(first_index);
